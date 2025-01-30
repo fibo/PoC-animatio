94a208a21c32787b474d61a3803b48499fbf964f
Gianluca Casati <fibo@users.noreply.github.com>
2025-01-30 12:47:07 +0700

    bouncing ball with sound

---
+++ assets/utils.js
@@ -0,0 +1,74 @@
+/**
+ * Create an SVG element.
+ *
+ * @example
+ *
+ * ```js
+ * const circle = createSvgElement('circle')
+ *   .set('cx', 100)
+ *   .set('cy', 100)
+ *
+ * console.log(circle.get('cx'))
+ * ```
+ *
+ * @param {string} qualifiedName
+ */
+export function createSvgElement(qualifiedName) {
+  var element = document.createElementNS('http://www.w3.org/2000/svg', qualifiedName)
+  return Object.assign(element, {
+    get: (attributeName) => element.getAttribute(attributeName),
+    set: (attributeName, value) => {
+      element.setAttribute(attributeName, value)
+      return element
+    }
+  })
+}
+
+/**
+ * Create an SVG.
+ *
+ * @example
+ *
+ * ```js
+ * const svg = createSvg(100, 200)
+ * ```
+ *
+ * @param {number} width
+ * @param {number} height
+ */
+export function createSvg(width, height) {
+  var svg = createSvgElement('svg').set('xmlns', 'http://www.w3.org/2000/svg')
+  return Object.assign(svg, {
+    resize: (width, height) =>
+      svg.set('width', width)
+         .set('height', height)
+         .set('viewBox', `0 0 ${width} ${height}`)
+  }).resize(width, height)
+}
+
+/** Round number to two decimals. */
+export function decimal2(value) {
+  return Number(value.toFixed(2))
+}
+
+/**
+ * Create an HTML template element from a string template.
+ *
+ * @example
+ *
+ * ```js
+ * const myTemplate = html`
+ *   <style>
+ *     :host {
+ *       display: block;
+ *     }
+ *   </style>
+ *   <slot></slot>
+ * `
+ * ```
+ */
+export function html(strings, ...expressions) {
+  var template = document.createElement('template')
+  template.innerHTML = strings.reduce((result, str, i) => result + str + (expressions[i] ?? ''), '')
+  return template
+}
--- bouncing-ball.js
+++ bouncing-ball.js
@@ -1,40 +1,81 @@
-import { createElementSvg, html } from './utils.js'
-
-const containerSize = 200
+import { createSvg, createSvgElement, decimal2, html } from './assets/utils.js'
 
 class BouncingBall extends HTMLElement {
   static localName = 'bouncing-ball'
+  static strokeWidth = 2
+  static containerWidth = 150
+  static containerHeight = 200
   static template = html`
     <style>
       :host {
         display: block;
-        width: ${containerSize}px;
-        height: ${containerSize}px;
+        width: ${BouncingBall.containerWidth}px;
+        height: ${BouncingBall.containerHeight}px;
         cursor: pointer;
-        border: 1px solid;
+        border: 2px solid gainsboro;
+        border-radius: var(--radius-s);
+      }
+      :host(:not([data-action])) {
+        cursor: not-allowed;
+      }
+      ellipse {
+        fill: orange;
+        stroke-width: ${BouncingBall.strokeWidth}px;
+        stroke: black;
+        transition: opacity 250ms;
+      }
+      ellipse.disposing {
+        opacity: 0.4;
       }
     </style>
-    <svg
-      viewBox="0 0 ${containerSize} ${containerSize}"
-      width=${containerSize}
-      height=${containerSize}
-      xmlns="http://www.w3.org/2000/svg"></svg>
+    <audio src="./assets/basketball.wav"></audio>
   `
-  ball = createElementSvg('ellipse')
+  ball = createSvgElement('ellipse')
+  /** @type {'stopped'|'playing'|'disposing'} */
   status = 'stopped'
+  /** Frames per second. */
+  FPS = 30
+  /** Initial ball y coordinate. */
+  initialY = 30
+  /** Ball radius, excluding stroke-width. */
+  radius = 20
+  /** Duration of ball falling, in milliseconds. */
+  fallDuration = 1000
+  /** Duration of bll bouncing, in milliseconds. */
+  bounceDuration = 300
+  /**
+   * Indicates if the ball is bouncing on the floor.
+   * During this stage, the ball will deform due to elasticity.
+   */
+  isBouncing = false
+  /**
+   * Ball velocity direction.
+   * @type {'up'|'down'}
+   */
+  direction = 'down'
+  /** During bouncing the radius ball will stretch up to this percentage. */
+  maxBallRadiusDeformationPercentage = .75
+
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
+    const { containerWidth, containerHeight } = BouncingBall
     this.shadowRoot.appendChild(BouncingBall.template.content.cloneNode(true))
-    const svg = this.svg = this.shadowRoot.querySelector('svg')
-    svg.setAttribute('viewBox', `0 0 ${containerSize} ${containerSize}`)
+    const svg = this.svg = createSvg(containerWidth, containerHeight)
     svg.appendChild(this.ball
-      .set('cx', containerSize / 2).set('cy', 30).set('rx', 20).set('ry', 20)
+      .set('cx', containerWidth / 2)
+      .set('cy', this.initialY)
+      .set('rx', this.radius)
+      .set('ry', this.radius)
     )
+    this.shadowRoot.appendChild(svg)
   }
+
   connectedCallback() {
+    this.dataset.action = 'start'
     this.svg.addEventListener('click', this)
   }
+
   handleEvent(event) {
     if (event.type == 'click') {
       // Toggle animation.
@@ -42,12 +83,137 @@ class BouncingBall extends HTMLElement {
       else if (this.status == 'playing') this.stop()
     }
   }
+
+  /** Must be called during some user interaction, e.g. click. */
+  setupAudio() {
+    // Do nothing if audio was already initialized.
+    if (this.audioElement) return
+    // Create audio context and connect audio element.
+    this.audioElement = this.shadowRoot.querySelector('audio')
+    const audioContext = new AudioContext()
+    audioContext
+      .createMediaElementSource(this.audioElement)
+      .connect(audioContext.destination)
+  }
+
   play() {
     this.status = 'playing'
+    this.dataset.action = 'stop'
+    this.setupAudio()
+
+    const numFramesOfBouncing = Math.floor(this.bounceDuration / this.deltaT)
+    const deltaR = this.radius * this.maxBallRadiusDeformationPercentage / numFramesOfBouncing / 2
+    const distance = Math.floor(BouncingBall.containerHeight - this.initialY - this.radius)
+    // The deltaY with no acceleration.
+    const uniformDeltaY = Math.floor(distance * this.deltaT / this.fallDuration)
+
+    async function* animate(self) {
+      while (true) {
+        const currentY = Number(self.ball.get('cy'))
+        if (self.isBouncing) {
+//// Bounce animation.
+          let i = 1
+          while(i <= numFramesOfBouncing) {
+            let ry = Number(self.ball.get('ry'))
+            if (i <= numFramesOfBouncing / 2) ry -= deltaR
+            else ry += deltaR
+            // Keep the same area.
+            let rx = self.radius * self.radius / ry
+            // Round to two decimals.
+            rx = decimal2(rx)
+            ry = decimal2(ry)
+            // Deform the ball.
+            self.ball.set('ry', ry)
+            self.ball.set('rx', rx)
+            // Move center in order to always touch the floor.
+            self.ball.set('cy', decimal2(BouncingBall.containerHeight - ry - BouncingBall.strokeWidth))
+            i++
+            yield Promise.resolve()
+          }
+          // Once bounce is finished, restore original ball radius.
+          self.ball.set('rx', self.radius).set('ry', self.radius)
+          self.isBouncing = false
+          self.direction = 'up'
+        } else if (currentY < self.initialY) {
+//// Ball returned to the top.
+          self.direction = 'down'
+          // Do not go above the container.
+          self.ball.set('cy', self.initialY)
+          // Stop animation if status is disposing.
+          if (self.status == 'disposing') {
+            self.status = 'stopped'
+            return Promise.resolve()
+          }
+        } else {
+//// Compute deltaY.
+          let deltaY
+          if (self.status == 'disposing')
+            // Move with constant velocity during disposure.
+            deltaY = 2 * uniformDeltaY
+          else
+            // A sort of acceleration.
+            deltaY = Math.max(1, Math.floor(uniformDeltaY * (currentY - self.initialY) / 7))
+          // Adjust vector direction.
+          if (self.direction == 'up') deltaY = -deltaY
+//// Check if it is a hit.
+          if (!self.isBouncing && (currentY + self.radius + deltaY > BouncingBall.containerHeight)) {
+            // Do not go below the container.
+            self.ball.set('cy', decimal2(BouncingBall.containerHeight - self.radius - BouncingBall.strokeWidth))
+            // Play bouncing sound.
+            self.audioElement.play()
+            // Start bouncing.
+            self.isBouncing = true
+          } else {
+//// Move the ball.
+          self.ball.set('cy', decimal2(currentY + deltaY))
+          }
+        }
+        yield Promise.resolve()
+      }
+    }
+
+    const animation = animate(this)
+
+    const loop = async () => {
+      // Next animation frame can run when
+      // current time is greater than last frame time plus frame duration.
+      if (document.timeline.currentTime > this.lastFrameTime + this.deltaT) {
+        await animation.next()
+        this.lastFrameTime = document.timeline.currentTime
+      }
+      if (this.status == 'stopped') {
+        this.dataset.action = 'start'
+        this.ball.classList.remove('disposing')
+        cancelAnimationFrame(this.frameRequestId)
+      } else {
+        this.frameRequestId = requestAnimationFrame(loop)
+      }
+    }
+    // Will start animation immediatly.
+    // Using
+    //
+    //     this.lastFrameTime = document.timeline.currentTime
+    //
+    // would start animation after 1 frame duration.
+    this.lastFrameTime = document.timeline.currentTime - this.deltaT
+    loop()
   }
+
   stop() {
-    this.status = 'stopped'
+    this.status = 'disposing'
+    this.ball.classList.add('disposing')
+    this.direction = 'up'
+    delete this.dataset.action
+  }
+
+  /** Frame duration in milliseconds. */
+  get deltaT() {
+    return Math.floor(1000 / this.FPS)
   }
 }
 
-customElements.define(BouncingBall.localName, BouncingBall)
+addEventListener('load', () => {
+  customElements.define(BouncingBall.localName, BouncingBall)
+
+
+})
